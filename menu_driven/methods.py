3f210b446af52ca7c5453111af567033717b0bf8
Sudat Sasim Shrestha <sudatsasimshrestha@gmail.com>
2023-08-11 09:47:44 +0545

    UnitTesting

--- menu_driven/methods.py
+++ menu_driven/methods.py
@@ -2,13 +2,18 @@ import socket
 import os
 
 import buffer
-def send():
+
+def send(socket_obj=None):
     HOST = '127.0.0.1'
     PORT = 2345
     connection = False
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c = s.connect((HOST, PORT))
-    print(c)
+
+    if socket_obj is None:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        c = s.connect((HOST, PORT))
+        print(c)
+    else:
+        s = socket_obj
 
     with s:
         sbuf = buffer.Buffer(s)
@@ -23,30 +28,30 @@ def send():
             sbuf.put_utf8(hash_type)
             sbuf.put_utf8(file_name)
 
-
             file_size = os.path.getsize(file_name)
-            print("{:.2f} MB".format(file_size/(1024*1024)))
+            print("{:.2f} MB".format(file_size / (1024 * 1024)))
             sbuf.put_utf8(str(file_size))
 
             with open(file_name, 'rb') as f:
                 sbuf.put_bytes(f.read())
             print('File Sent')
 
-def recieve():
+def receive(socket_obj=None):
     HOST = ''
     PORT = 2345
 
-    # If server and client run in same local directory,
-    # need a separate place to store the uploads.
     try:
         os.mkdir('downloads')
     except FileExistsError:
         pass
 
-    s = socket.socket()
-    s.bind((HOST, PORT))
-    s.listen(10)
-    print("Waiting for a connection.....")
+    if socket_obj is None:
+        s = socket.socket()
+        s.bind((HOST, PORT))
+        s.listen(10)
+        print("Waiting for a connection.....")
+    else:
+        s = socket_obj
 
     while True:
         conn, addr = s.accept()
@@ -65,29 +70,29 @@ def recieve():
                 print("Wrong file or connection closed.")
                 break
             file_name = file_name.split('/')[-1]
-            file_name = os.path.join('downloads/',file_name)
-            print('file name: ', file_name) 
-            print('file size: {:.2f} MB'.format(file_size/(1024*1024)))
+            file_name = os.path.join('downloads/', file_name)
+            print('file name: ', file_name)
+            print('file size: {:.2f} MB'.format(file_size / (1024 * 1024)))
 
             with open(file_name, 'wb') as f:
                 remaining = file_size
                 while remaining:
                     chunk_size = 4096 if remaining >= 4096 else remaining
                     chunk = connbuf.get_bytes(chunk_size)
-                    if not chunk: break
+                    if not chunk:
+                        break
                     f.write(chunk)
                     remaining -= len(chunk)
-                    #print("\rSent: {:.2f} MB".format((file_size-remaining)/(1024*1024)))
                 if remaining:
-                    print('File incomplete.  Missing',remaining,'bytes.')
+                    print('File incomplete. Missing', remaining, 'bytes.')
                 else:
                     print('File received successfully.')
         print('Connection closed.')
         conn.close()
 
 if __name__ == "__main__":
-    ask = int(input("What do you want?\n1. Send\n2. Recieve\n\n"))
-    if ask is 1:
+    ask = int(input("What do you want?\n1. Send\n2. Receive\n\n"))
+    if ask == 1:
         send()
-    elif ask is 2:
-        recieve()
+    elif ask == 2:
+        receive()
